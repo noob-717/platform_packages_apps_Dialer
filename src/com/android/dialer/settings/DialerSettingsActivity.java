a4cd2a882208ce06f9d6c99ed38ac3ea2e37453e
BeansTown106 <nbenis106@gmail.com>
2016-06-09 08:39:41 -0400

    removed cyngn,whitepages,yellowpages lookups *removed cyngn chineese lookup as it only pertains to cyanogen os roms *remove whitepages/yellowpages as they are blocking requests to the api *also remove lookup description looks out of place
    Change-Id: I5e192a63ae3a6bbe72a66b42374858ff97c65852

--- src/com/android/dialer/settings/DialerSettingsActivity.java
+++ src/com/android/dialer/settings/DialerSettingsActivity.java
@@ -86,7 +86,6 @@ public class DialerSettingsActivity extends AppCompatPreferenceActivity {
 
         final Header lookupSettingsHeader = new Header();
         lookupSettingsHeader.titleRes = R.string.lookup_settings_label;
-        lookupSettingsHeader.summaryRes = R.string.lookup_settings_description;
         lookupSettingsHeader.fragment = LookupSettingsFragment.class.getName();
         target.add(lookupSettingsHeader);
 
